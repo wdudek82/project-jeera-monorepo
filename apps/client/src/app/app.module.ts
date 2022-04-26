7364400f502e20f7edf840fa7a3d53d1f76672bd
Wojciech Dudek <wdudek82@gmail.com>
2022-04-27 01:22:32 +0200

    Remove redundant test component

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -4,11 +4,10 @@ import { CoreModule } from '@client/core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from '@client/auth/auth.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BarComponent } from './bar/bar.component';
 
 @NgModule({
   imports: [CoreModule, AppRoutingModule, AuthModule, BrowserAnimationsModule],
-  declarations: [AppComponent, BarComponent],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
